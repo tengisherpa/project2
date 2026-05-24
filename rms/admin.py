7b519295da888c81db0cd7010e3b9d5d51590e60
Tengi <stengi558@gmail.com>
2026-05-24 21:25:55 +0545

    models create

--- rms/admin.py
+++ rms/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['id','name']
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display=['id','name','price','category']
+    list_filter=['category']
+    search_fields=['name']
+    list_per_page=10    
+admin.site.register(Food,FoodAdmin)
+
+
+class TableAdmin(admin.ModelAdmin):
+    list_display=['id','number','capacity','is_available']
+    search_fields=['number','capacity','is_available']
+admin.site.register(Table,TableAdmin)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    autocomplete_fields=['food']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','total_price','status','payment_status','user']
+    list_filter=['status','payment_status']
+    search_fields=['user_username']
+    list_per_page=10 
+    inlines=[OrderItemInline]
+admin.site.register(Order,OrderAdmin)
+
+# class OrderItemAdmin(admin.ModelAdmin):
+#     list_display=['id','order','food']
+#     list_filter=['order']
+#     search_fields=['food_name']
+#     list_per_page=10 
+# admin.site.register(OrderItem,OrderItemAdmin)
+
+
+
--- rms/models.py
+++ rms/models.py
@@ -1,35 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
 class Category(models.Model):
     name=models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
     
 class Food(models.Model):
     name=models.CharField(max_length=225)
     description=models.TextField()
     price=models.IntegerField()
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.name}-Rs.{self.price}"
     
 class Table(models.Model):
     number=models.CharField(max_length=2)
     capacity=models.CharField(max_length=2)
-    is_available=models.BooleanField()
-    (default=True)
+    is_available=models.BooleanField(default=True)
     
 class Order(models.Model):
     status_choice=[
-        ('pending','Pending'),
-        ('completed','Completed'),
-        ('delivered','Delivered')
+        ('P','Pending'),
+        ('C','Completed'),
+        ('D','Delivered')
     ]
     payment_status=[
         ('p','PAID'),
         ('U','PENDING')
     ]
-    user = models.ForeignKey(User,on_delete=models.Cascade)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     total_price=models.IntegerField()
-    status=models.CharField(max_length=100,choices=status_choice)
+    status=models.CharField(max_length=100,choices=status_choice,default='P')
     payment_status=models.CharField(max_length=1,choices=payment_status,default='P')
+    
     
 class OrderItem(models.Model):
     order=models.ForeignKey(Order,on_delete=models.PROTECT)
